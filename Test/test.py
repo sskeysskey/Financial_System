213634037ea1821e5fffce352c08e1bd4869ea63
sskeysskey <728308386@qq.com>
2024-10-18 15:02:02 +0800

    Auto commit at 2024-10-18 15:02:02

--- Test/test.py
+++ Test/test.py
@@ -1,30 +1,9 @@
-import sqlite3
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
 
-# 连接到SQLite数据库
-conn = sqlite3.connect('/Users/yanzhang/Documents/Database/Finance.db')
-cursor = conn.cursor()
+def index():
+    return 'Hello World'
 
-# 股票名称和拆股比例
-name = 'SCHG'
-price_divisor = 4
-
-# 获取该股票的最新日期
-cursor.execute("""
-    SELECT MAX(date) 
-    FROM ETFs 
-    WHERE name = ?
-""", (name,))
-latest_date = cursor.fetchone()[0]  # 获取最新日期
-
-# 执行拆股操作，排除最新日期的数据
-cursor.execute("""
-    UPDATE ETFs 
-    SET price = ROUND(price / ?, 2) 
-    WHERE name = ? AND date < ?
-""", (price_divisor, name, latest_date))
-
-# 提交更改
-conn.commit()
-
-# 关闭数据库连接
-conn.close()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
