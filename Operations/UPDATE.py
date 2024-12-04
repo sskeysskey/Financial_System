f6b25ff3b99c97b90a5dcb4f98ac1a03d7fa65df
sskeysskey <728308386@qq.com>
2024-12-04 15:18:20 +0800

    Auto commit at 2024-12-04 15:18:20

--- Operations/UPDATE.py
+++ Operations/UPDATE.py
@@ -25,7 +25,7 @@ cursor = conn.cursor()
 # """, (price_multiplier, name))
 
 # 更新价格字段
-cursor.execute("UPDATE Commodities SET price = 15.22 WHERE id = 103901")
+# cursor.execute("UPDATE Commodities SET price = 15.22 WHERE id = 103901")
 # cursor.execute("UPDATE Currencies SET date = '2024-11-14' WHERE id = 143561")
 
 # 保留两位小数
