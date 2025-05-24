5829b7ab087cb954d83f40d61864bd18a998fec0
sskeysskey <728308386@qq.com>
2025-05-24 18:23:48 +0800

    Auto commit at 2025-05-24 18:23:48

--- Backup/csv_chartview.py
+++ Backup/csv_chartview.py
@@ -8,13 +8,13 @@ def plot_financial_data(csv_file_path, start_date_str=None, end_date_str=None):
         df = pd.read_csv(
             csv_file_path,
             encoding='utf-8-sig',    # 万一有 BOM
-            parse_dates=['date']    # 这时就能正常识别 date 列
+            parse_dates=['Date']    # 这时就能正常识别 date 列
         )
         
         # 2）后续保持不变
-        df.set_index('date', inplace=True)
-        df['value'] = pd.to_numeric(df['value'], errors='coerce')
-        df.dropna(subset=['value'], inplace=True)
+        df.set_index('Date', inplace=True)
+        df['Value'] = pd.to_numeric(df['Value'], errors='coerce')
+        df.dropna(subset=['Value'], inplace=True)
         if df.empty:
             print("错误：没有可用数据。")
             return
@@ -34,7 +34,7 @@ def plot_financial_data(csv_file_path, start_date_str=None, end_date_str=None):
         plt.figure(figsize=(12,6))
         plt.plot(
     df.index,
-    df['value'],
+    df['Value'],
     linestyle='-',
     marker='o',
     color='b',
@@ -64,5 +64,5 @@ def plot_financial_data(csv_file_path, start_date_str=None, end_date_str=None):
         print(f"处理文件时发生错误：{e}")
 
 if __name__ == '__main__':
-    file_path = '/Users/yanzhang/Downloads/3.csv'
-    plot_financial_data(file_path, start_date_str='2015-01-06', end_date_str='2025-01-01')
+    file_path = '/Users/yanzhang/Downloads/Firstrade/Deals.csv'
+    plot_financial_data(file_path, start_date_str='2023-01-01', end_date_str='2024-01-01')
